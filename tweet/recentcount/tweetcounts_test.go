8d1e516d80e60b79cc84c0957c463a08933de0fb
Green Orange <greenorangebay@yahoo.com>
2021-08-29 18:56:48 +0200

    Removed keys

--- tweet/recentcount/tweetcounts_test.go
+++ tweet/recentcount/tweetcounts_test.go
@@ -4,12 +4,10 @@ import (
 	"os"
 	"testing"
 
-	"github.com/0dayfall/carboncopy/config"
 	"github.com/0dayfall/carboncopy/httphandler"
 )
 
 func TestMain(m *testing.M) {
-	config.Init("AAAAAAAAAAAAAAAAAAAAAI0kRgEAAAAAypS5hDlUu0fQxhPfsegcVRKgGyE%3Dz5LtZJLTBtN5xgrCCGAvQPX8a8fZFxkKJVhWCHkHkIEoSDCcvM")
 	exitVal := m.Run()
 	os.Exit(exitVal)
 }
